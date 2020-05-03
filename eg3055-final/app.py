d316a0223685f4ce4d73e34b298ecc9a02f444ec
elizabethg60 <64707754+elizabethg60@users.noreply.github.com>
2020-05-03 11:05:01 -0400

    added external url

---
+++ eg3055-final/app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 14:57:17 2020
+
+@author: etill
+"""
+
+#import statements
+from flask import Flask, render_template
+
+#Flask app variable
+app = Flask(__name__)
+
+#static route
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+#classes link
+@app.route("/classes")
+def classes():
+    return render_template('classes.html')
+
+#assignments link
+@app.route("/assignments")
+def assignments():
+    return render_template('assignments.html')
+    
+    
+#start the server
+if __name__ == "__main__":
+    app.run()
